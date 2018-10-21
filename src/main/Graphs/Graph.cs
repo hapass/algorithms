7628e65070f65c4770bec56c489bb49b7dd69725
Pavel Zavoiskikh <hapass@ya.ru>
2018-10-21 23:26:04 +0300

    Depth first search.

--- src/main/Graphs/Graph.cs
+++ src/main/Graphs/Graph.cs
@@ -5,28 +5,49 @@ namespace Graphs
 {
     public class Graph
     {
-        private Dictionary<String, List<String>> nodeConnections;
+        private Dictionary<String, List<String>> adjacentNodesMap;
+        private HashSet<String> reachableNodes;
 
         public Graph()
         {
-            this.nodeConnections = new Dictionary<string, List<string>>();
+            this.adjacentNodesMap = new Dictionary<string, List<string>>();
+            this.reachableNodes = new HashSet<string>();
         }
 
         public Graph AddEdge(string fromNode, string toNode)
         {
-            if(!this.nodeConnections.ContainsKey(fromNode))
+            if(!this.adjacentNodesMap.ContainsKey(fromNode))
             {
-                this.nodeConnections.Add(fromNode, new List<string>());
+                this.adjacentNodesMap.Add(fromNode, new List<string>());
             }
 
-            this.nodeConnections[fromNode].Add(toNode);
+            if(!this.adjacentNodesMap.ContainsKey(toNode))
+            {
+                this.adjacentNodesMap.Add(toNode, new List<string>());
+            }
+
+            this.adjacentNodesMap[fromNode].Add(toNode);
 
             return this;
         }
 
         public bool HasPath(string node, string toNode)
         {
-            return true;
+            DepthFirstSearch(node);
+            return reachableNodes.Contains(toNode);
+        }
+
+        private void DepthFirstSearch(string node)
+        {
+            var adjacentNodes = this.adjacentNodesMap[node];
+            foreach(var adjacentNode in adjacentNodes)
+            {
+                if(!reachableNodes.Contains(adjacentNode))
+                {
+                    reachableNodes.Add(adjacentNode);
+                    DepthFirstSearch(adjacentNode);
+                }
+            }
         }
     }
 }
