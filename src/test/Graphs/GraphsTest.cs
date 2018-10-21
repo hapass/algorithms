92219cdceb49f2b0f6668c8e8a6465c25d81ddc8
Pavel Zavoiskikh <hapass@ya.ru>
2018-10-21 23:25:32 +0300

    Depth first search.

--- src/test/Graphs/GraphsTest.cs
+++ src/test/Graphs/GraphsTest.cs
@@ -6,7 +6,7 @@ namespace Graphs
     public class  GraphsTest
     {
         [Fact]
-        public void GraphSaysNodesAreConnectedWhenTheyAreConnected()
+        public void GraphSaysThereIsAPathFromNodeToNodeWhenThereIsAPath()
         {
             var graph = new Graph();
             graph.AddEdge("1", "2");
@@ -14,6 +14,17 @@ namespace Graphs
             var hasPath = graph.HasPath("1", "2");
 
             Assert.True(hasPath);
+        }
+
+        [Fact]
+        public void GraphSaysThereIsNoPathFromNodeToNodeWhenThereIsNoPath()
+        {
+            var graph = new Graph();
+            graph.AddEdge("1", "2");
+
+            var hasPath = graph.HasPath("2", "1");
+
+            Assert.False(hasPath);
         }
     }
 }
